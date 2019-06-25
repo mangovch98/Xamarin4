ba35406c410571c242328666c376c10adf002452
Mauricio Vásquez Chalup <mauriciovch98@gmail.com>
2019-06-25 14:34:41 -0700

    carpeta models completa

---
+++ app/app/app/Models/Paises.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;              
+
+namespace app.Models
+{
+
+    class Paises
+    {
+        #region Atributos
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "population")]        
+        public int Population { get; set; }
+
+        [JsonProperty(PropertyName = "capital")]
+        public string Capital { get; set; }
+        #endregion
+    }
+}
---
+++ app/app/app/Models/Response.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app.Models
+{
+
+    public class Response
+    {
+        public bool IsSuccess
+        {
+            get;
+            set;
+        }
+
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        public object Result
+        {
+            get;
+            set;
+        }
+    }
+
+}
